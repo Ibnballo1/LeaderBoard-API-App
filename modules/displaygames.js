a9668d88a2cb98dac3535a1721b805ea2ea0f09d
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-23 20:48:57 +0100

    Don't know what changes happened

--- modules/displaygames.js
+++ modules/displaygames.js
@@ -1,5 +1,5 @@
 // Displaying the games on the page
-const mainList = document.getElementById('main-list');
+export const mainList = document.getElementById('main-list');
 
 const displayGames = (games) => {
   games.forEach((game, index) => {
@@ -7,7 +7,11 @@ const displayGames = (games) => {
     const eachDiv = document.createElement('div');
     const para = document.createElement('p');
 
-    eachDiv.className = 'eachScore white-bg';
+    if (index % 2 === 0) {
+      eachDiv.className = 'eachScore white-bg';
+    } else {
+      eachDiv.className = 'eachScore grey-bg';
+    }
     para.className = 'users user-1';
     para.textContent = `${game['user']}: ${game['score']}`;
     eachDiv.appendChild(para);
