9a8b07ccc8015ba64df2d20b91fff74bd78a28d4
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-23 18:36:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './style.css';
-
+import displayGames from '../modules/displaygames.js';
 // Fetch data from API
 const getData = async () => {
   const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/';
   const request = new Request(url);
   const results = await fetch(request);
-  const gameObj = await resu.json();
+  const gameObj = await results.json();
   const { result } = gameObj;
-  console.log(result);
+  //console.log(result.length);
+  displayGames(result)
 }
 getData();
