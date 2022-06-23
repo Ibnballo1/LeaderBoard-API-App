3092140f9e538edcd172a6a31d6b15389f094625
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-23 16:51:06 +0100

    Fetch data from Api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+// Fetch data from API
+const getData = async () => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/';
+  const request = new Request(url);
+  const results = await fetch(request);
+  const gameObj = await resu.json();
+  const { result } = gameObj;
+  console.log(result);
+}
+getData();
