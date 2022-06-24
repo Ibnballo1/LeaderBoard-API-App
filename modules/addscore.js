d1304927da1f36d4b0c5788ab9280e95543734ed
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-24 06:18:32 +0100

    Update addscore.js

--- modules/addscore.js
+++ modules/addscore.js
@@ -1,8 +1,14 @@
-class AddGame {
-    constructor(user, score) {
-      this.user = user;
-      this.score = score;
-    }
-  }
-    
-  export default AddGame;
+import displayGames from '../modules/displaygames.js';
+
+// Fetch data from API
+const getData = async () => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/g85gOOK1Kvia0Pysqqy3/scores';
+  const request = new Request(url);
+  const results = await fetch(request);
+  const gameObj = await results.json();
+  const { result } = gameObj;
+  //console.log(result.length);
+  displayGames(result)
+}
+
+export default getData;
